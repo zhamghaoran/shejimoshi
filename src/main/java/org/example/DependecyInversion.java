93ea304071bb40e2f13b3e8c48f43a8ad204fa2f
zhanghaoran <2017929959@qq.com>
2022-11-11 10:23:16 +0800

    ""

--- src/main/java/org/example/DependecyInversion.java
+++ src/main/java/org/example/DependecyInversion.java
@@ -1,2 +1,42 @@
-package org.example;public class DependecyInversion {
+package org.example;
+
+public class DependecyInversion {
+    public static void main(String[] args) {
+        ChangHong changHong = new ChangHong();
+        OpenAndClose openAndClose = new OpenAndClose(changHong);
+        openAndClose.open(changHong);
+
+    }
+}
+
+// 方式1.通过接口传递实现依赖
+// 开关的接口
+interface IOpenAndClose {
+    public void open(ITV itv);
+}
+
+interface ITV {
+    public void play();
+}
+class ChangHong implements ITV {
+
+    @Override
+    public void play() {
+        System.out.println("长虹电视打开");
+    }
+}
+class OpenAndClose implements IOpenAndClose {
+    private ITV itv;
+    public OpenAndClose(ITV itv) {
+        this.itv = itv;
+    }
+
+    @Override
+    public void open(ITV itv) {
+        this.itv.play();
+    }
+
+    public void setItv(ITV itv) {
+        this.itv = itv;
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,35 @@
-package org.example;public class Main {
+package org.example;
+
+/**
+ * 1.简单比较容易想到
+ * 2.如果我们获取的对象是 微信，短信等，则新增类，同时person也要增加相应的接受方法
+ * 3.解决方法:引入一个抽象的接口IReceiver，表示接受者，这样Person类与接口IReceiver发生依赖
+ *  因为Email，WeiXin 等等属于接受的范畴，他们各自实现IReceiver接口就ok，这样我们就完成了依赖倒置
+ */
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Person person = new Person();
+        person.receive(new Email());
+        person.receive(new WeiXin());
+    }
+}
+interface IReceiver {
+    public String getInfo();
+}
+class Email implements IReceiver{
+    public String getInfo() {
+        return "电子邮件信息: hello world";
+    }
+}
+class WeiXin implements IReceiver {
+
+    @Override
+    public String getInfo() {
+        return "微信消息: hello ,ok";
+    }
+}
+class Person {
+    public void receive(IReceiver iReceiver) {
+        System.out.println(iReceiver.getInfo());
     }
 }
